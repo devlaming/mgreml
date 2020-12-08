96d3bd5c54626e49eb4b40424aef0e08c197d56f
devlaming <devlaming@protonmail.com>
2020-12-08 16:28:06 +0100

    Expanded on reader.py

--- mgreml/data/reader.py
+++ mgreml/data/reader.py
@@ -1,10 +1,37 @@
 import pandas as pd
 import numpy as np
 import logging
+import time
+from data import tools
+
+__version__ = '0.01'
+MASTHEAD  = "████████████████████████████████████████████████████████████████████████\n"
+MASTHEAD += "██                                                                    ██\n"
+MASTHEAD += "██    ██      ██                                                      ██\n"
+MASTHEAD += "██    ███    ███  ██████   ██████    █████████ ██     ██ ██           ██\n"
+MASTHEAD += "██    ████  ████ ██    ██  ██    ██  ██        ███   ███ ██           ██\n"
+MASTHEAD += "██    ██ ████ ██ ██        ██    ██  ██        ████ ████ ██           ██\n"
+MASTHEAD += "██    ██  ██  ██ ██   ████ ██████    ███████   ██ ███ ██ ██           ██\n"
+MASTHEAD += "██    ██      ██ ██    ██  ██   ██   ██        ██  █  ██ ██           ██\n"
+MASTHEAD += "██    ██      ██ ██    ██  ██    ██  ██        ██     ██ ██           ██\n"
+MASTHEAD += "██    ██      ██  ██████   ██     ██ █████████ ██     ██ █████████    ██\n"
+MASTHEAD += "██                                                                    ██\n"
+MASTHEAD += "██                                                                    ██\n"
+MASTHEAD += "████████████████████████████████████████████████████████████████████████\n"
+MASTHEAD += "██                                                                    ██\n"
+MASTHEAD += "██  VERSION {V}                                                      ██\n".format(V=__version__)
+MASTHEAD += "██                                                                    ██\n"
+MASTHEAD += "██  (C) 2020 Ronald de Vlaming, Eric Slob, Philip Jansen,             ██\n"
+MASTHEAD += "██  Philipp Koellinger, Patrick Groenen, Niels Rietveld               ██\n"
+MASTHEAD += "██                                                                    ██\n"
+MASTHEAD += "██  Vrij Universiteit Amsterdam and Erasmus University Rotterdam      ██\n"
+MASTHEAD += "██  GNU General Public License v3                                     ██\n"
+MASTHEAD += "██                                                                    ██\n"
+MASTHEAD += "████████████████████████████████████████████████████████████████████████\n"
 
 class MgremlReader:
     
-    def __init__(self, parser, logger):
+    def __init__(self, parser, logger, dStart):
         '''
         MgremlReader is provided with a logger and the input arguments
         from the call of the main mgreml script e.g.
@@ -18,11 +45,14 @@ class MgremlReader:
         MGREML estimation; the attributes of the MgremlReader will be 
         used to determine what needs to be done
         '''
-        # store logger and parser as attributes of instance
+        # store logger, parser and starting time as attributes of instance
         self.logger = logger
         self.parser = parser
+        self.dStart = dStart
         # initialise arguments
         self.InitialiseArgsAndLogger()
+        # print welcome screen and given input args
+        self.PrintStart()
         # get DataFrame for GRM based on --grm mygrm option
         self.read_grm()
         self.sPrefix        # string based on --out results
@@ -68,31 +98,39 @@ class MgremlReader:
         self.parser.add_argument('--grm', default=None, type=str,
                             help='prefix of the binary GRM from GCTA. NB: case insensitive.')
         self.parser.add_argument('--pheno', default=None, type=str,nargs='*',
-                            help='Name of the phenotypes file. NB: case insensitive.')
+                            help='Name of the phenotypes file. NB: case insensitive. Possible to add the flags nolabelpheno; not recommended, always name things.')
         self.parser.add_argument('--covar', default=None, type=str,nargs='*',
-                            help='Name of the covariates file. NB: case insensitive.')
+                            help='Name of the covariates file. NB: case insensitive. Possible to add the flags nolabelcovar; not recommended, always name things.')
         self.parser.add_argument('--covar-model', default=None, type=str,nargs='*',
-                            help='Name of a file that has the specification for which covariate affects which phenotype. Possible to add the flags nolabelpheno and/or nolabelcovar.')
+                            help='Name of a file that specifies which covariates (columns) affect which phenotypes (rows). Possible to add the flags nolabelpheno and/or nolabelcovar; not recommended, always name things.')
+        self.parser.add_argument('--rel-cutoff', default=None, type=int,
+                            help='Relatedness cut-off to apply to GRM. No cut-off is applied by default.')
+        self.parser.add_argument('--no-se',
+                            help='Option to indicate no standard errors should be reported (e.g. when only interested in a likelihood-ratio test for a nested model)')
         groupGenetic.add_argument('--genetic-model', default=None, type=str,nargs='*',
-                            help='Name of a file that has the specification for which  genetic factors apply to which phenotypes. Possible to add the flags nolabelpheno and/or nolabelfactor.')
+                            help='Name of a file that specifies which genetic factors (columns) affect which phenotypes (rows). Possible to add the flags nolabelpheno and/or nolabelfactor; not recommended, always name things.')
+        groupGenetic.add_argument('--rho-genetic', default=None, type=int,nargs='1',
+                            help='Integer equal to zero or one, forcing all genetic correlations to take on the specified value.')
         groupRestrictedGenetic.add_argument('--restricted-genetic-model', default=None, type=str,nargs='*',
-                            help='Name of a file that has the restricted specification for which  genetic factors apply to which phenotypes. Possible to add the flags nolabelpheno and/or nolabelfactor.')
+                            help='Name of a file that specifies for a restricted model which genetic factors (columns) affect which phenotypes (row). Possible to add the flags nolabelpheno and/or nolabelfactor; not recommended, always name things.')
+        groupRestrictedGenetic.add_argument('--restricted-rho-genetic', default=None, type=int,nargs='1',
+                            help='Integer equal to zero or one, forcing all genetic correlations in a restricted model to take on the specified value.')
         groupEnvironment.add_argument('--environment-model', default=None, type=str,nargs='*',
-                            help='Name of a file that has the specification for which  environmental factors apply to which phenotypes. Possible to add the flags nolabelpheno and/or nolabelfactor.')
+                            help='Name of a file that specifies which environment factors (columns) affect which phenotypes (rows). Possible to add the flags nolabelpheno and/or nolabelfactor; not recommended, always name things.')
+        groupEnvironment.add_argument('--rho-environment', default=None, type=int,nargs='1',
+                            help='Integer that must equal zero if option is used, forcing all environment correlations to zero.')
+        groupRestrictedEnvironment.add_argument('--restricted-rho-environment', default=None, type=int,nargs='1',
+                            help='Integer that must equal zero if option is used, forcing all environment correlations in a restricted model to zero.')
         groupRestrictedEnvironment.add_argument('--restricted-environment-model', default=None, type=str,nargs='*',
-                            help='Name of a file that has the restricted specification for which  environmental factors apply to which phenotypes. Possible to add the flags nolabelpheno and/or nolabelfactor.')
-        groupGenetic.add_argument('--rho-genetic', default=None, type=str,nargs='1',
-                            help='Integer 0 or 1 to identify no genetic correlation (0) and perfect genetic correlation with 1 factor (1).')
-        groupRestrictedGenetic.add_argument('--restricted-rho-genetic', default=None, type=str,nargs='1',
-                            help='Integer 0 or 1 to identify no genetic correlation (0) and perfect genetic correlation with 1 factor (1).')
-        groupEnvironment.add_argument('--rho-environment', default=None, type=str,nargs='1',
-                            help='Integer 0 to identify no environmental correlation (0).')
-        groupRestrictedEnvironment.add_argument('--restricted-rho-environment', default=None, type=str,nargs='1',
-                            help='Integer 0 to identify no environmental correlation (0).')
+                            help='Name of a file that specifies for a restricted model which environment factors (columns) affect which phenotypes (row). Possible to add the flags nolabelpheno and/or nolabelfactor; not recommended, always name things.')
+        # parse the input options
         self.args = self.parser.parse_args()
         # customise the logger e.g. usings args.out
         c_handler = logging.StreamHandler()
-        f_handler = logging.FileHandler(self.args.out + '.log','w+',encoding="utf-8")
+        if self.args.out is not None:
+            f_handler = logging.FileHandler(self.args.out + '.log','w+',encoding="utf-8")
+        else:
+            f_handler = logging.FileHandler('output.log','w+',encoding="utf-8")
         c_handler.setLevel(logging.DEBUG)
         f_handler.setLevel(logging.DEBUG)
         self.logger.setLevel(logging.DEBUG)
@@ -104,6 +142,47 @@ class MgremlReader:
         # Add handlers to the logger
         self.logger.addHandler(c_handler)
         self.logger.addHandler(f_handler)
+        
+    def PrintStart(self):
+        try:
+            defaults = vars(self.parser.parse_args(''))
+            opts = vars(self.args)
+            non_defaults = [x for x in opts.keys() if opts[x] != defaults[x]]
+            header = MASTHEAD
+            header += "\n\nCall: \n"
+            header += 'mgreml \\\n'
+            options = ['--'+x.replace('_','-')+' '+str(opts[x])+' \\' for x in non_defaults]
+            header += '\n'.join(options).replace('True','').replace('False','')
+            header = header[0:-1]+'\n'
+            self.logger.info(header)
+            if self.args.grm is not None:
+                dfA=read_grm(args,logger)
+            if self.args.pheno is not None:
+                dfY=read_pheno(args,logger)
+            if self.args.covar is not None:
+                dfX=read_covar(args,logger)    
+            if self.args.covar_model is not None:
+                dfBinXY=read_covar_model(args,logger)
+            if self.args.genetic_model is not None: 
+                dfGenBinFY=read_model(args,logger,component='genetic')
+            if self.args.restricted_genetic_model is not None: 
+                dfResGenBinFY=read_model(args,logger,component='genetic',restricted=True)    
+            if self.args.environment_model is not None: 
+                dfGenEnvFY=read_model(args,logger,component='environment')
+            if self.args.restricted_environment_model is not None: 
+                dfResEnvBinFY=read_model(args,logger,component='environment',restricted=True)   
+            if self.args.rho_genetic is not None:
+                dfGenBinFY=create_model(args,logger,component='genetic')
+            if len(vars(self.args))==1:
+                self.logger.warning('Error: no analysis selected.')
+                self.logger.info('mgreml_prepare.py -h describes options')
+        except Exception:
+            ex_type, ex, tb = sys.exc_info()
+            self.logger.error( traceback.format_exc(ex) )
+            raise   
+        finally:
+            self.logger.info('Total time elapsed: {T}'.format(
+                T=tools.sec_to_str(round(time.time() - self.dStart, 2))))
 
     def read_grm(self):
         # read binary GRM
