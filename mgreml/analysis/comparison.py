d77e3b9e89e601054bb2b29eef124ffbdaec0190
devlaming <devlaming@protonmail.com>
2020-12-08 08:37:20 +0100

    New notation models under null and alternative in NestedEstimator

--- mgreml/analysis/comparison.py
+++ mgreml/analysis/comparison.py
@@ -3,61 +3,60 @@ import pandas as pd
 from scipy.stats import chi2
 from mgreml.analysis import estimator
 
-
 class NestedEstimators:
     
-    def __init__(self, mdData, dfNestedGenBinFY = None, dfNestedEnvBinFY = None, dfGenBinFY = None, dfEnvBinFY = None, bBFGS = True, bStoreIters= False, bSEs = False, bReturnFullModelSpecs = False):
-        self.estimator_unres = estimator.MgremlEstimator(mdData, dfGenBinFY, dfEnvBinFY, bBFGS, bStoreIters, bSEs, bReturnFullModelSpecs)
-        self.estimator_res = estimator.MgremlEstimator(mdData, dfNestedGenBinFY, dfNestedEnvBinFY, bBFGS, bStoreIters, bSEs, bReturnFullModelSpecs)
+    def __init__(self, mdData, dfGenBinFY0 = None, dfEnvBinFY0 = None, dfGenBinFY = None, dfEnvBinFY = None, bBFGS = True, bStoreIters= False, bSEs = False, bReturnFullModelSpecs = False):
+        self.estimatorA = estimator.MgremlEstimator(mdData, dfGenBinFY, dfEnvBinFY, bBFGS, bStoreIters, bSEs, bReturnFullModelSpecs)
+        self.estimator0 = estimator.MgremlEstimator(mdData, dfGenBinFY0, dfEnvBinFY0, bBFGS, bStoreIters, bSEs, bReturnFullModelSpecs)
         self.CheckModelsNested()
         
     def CheckModelsNested(self):
         # find out which coefficients are free for genetic and env part
         # for restricted and unrestricted models
-        (mBGunres,mBEunres) = self.estimator_unres.mgreml_model.model.GetFreeCoeffs()
-        (mBGres,mBEres) = self.estimator_res.mgreml_model.model.GetFreeCoeffs()
+        (mBGA,mBEA) = self.estimatorA.mgreml_model.model.GetFreeCoeffs()
+        (mBG0,mBE0) = self.estimator0.mgreml_model.model.GetFreeCoeffs()
         # convert factor labels to pandas indices
-        indFGunres = pd.Index(self.estimator_unres.mgreml_model.model.genmod.lFactors)
-        indFEunres = pd.Index(self.estimator_unres.mgreml_model.model.envmod.lFactors)
-        indFGres = pd.Index(self.estimator_res.mgreml_model.model.genmod.lFactors)        
-        indFEres = pd.Index(self.estimator_res.mgreml_model.model.envmod.lFactors)
+        indFGA = pd.Index(self.estimatorA.mgreml_model.model.genmod.lFactors)
+        indFEA = pd.Index(self.estimatorA.mgreml_model.model.envmod.lFactors)
+        indFG0 = pd.Index(self.estimator0.mgreml_model.model.genmod.lFactors)        
+        indFE0 = pd.Index(self.estimator0.mgreml_model.model.envmod.lFactors)
         # find out if all factors in restricted genetic model
         # appear in unrestricted genetic model
-        if not(indFGres.isin(indFGunres).all()):
+        if not(indFG0.isin(indFGA).all()):
             raise ValueError('There is at least one genetic factor in your nested model that does not appear in your alternative model')        
         # find out if all factors in restricted environment model
         # appear in unrestricted environment model
-        if not(indFEres.isin(indFEunres).all()):
+        if not(indFE0.isin(indFEA).all()):
             raise ValueError('There is at least one environment factor in your nested model that does not appear in your alternative model')
         # select submatrices of free coeffs of unrestricted models
         # that align with free coeffs of restricted model
-        mBGunres_aligned = np.array(pd.DataFrame(mBGunres,columns=indFGunres)[indFGres])
-        mBEunres_aligned = np.array(pd.DataFrame(mBEunres,columns=indFEunres)[indFEres])
-        if ((mBGunres_aligned - mBGres) < 0).any():
+        mBGA_aligned = np.array(pd.DataFrame(mBGA,columns=indFGA)[indFG0])
+        mBEA_aligned = np.array(pd.DataFrame(mBEA,columns=indFEA)[indFE0])
+        if ((mBGA_aligned - mBG0) < 0).any():
             raise ValueError('There is at least one genetic coefficient where the nested model is free and the alternative model is not')
-        if ((mBEunres_aligned - mBEres) < 0).any():
+        if ((mBEA_aligned - mBE0) < 0).any():
             raise ValueError('There is at least one environment coefficient where the nested model is free and the alternative model is not')
-        self.iDF = int((mBGunres.sum() + mBEunres.sum()) - (mBGres.sum() + mBEres.sum()))
+        self.iDF = int((mBGA.sum() + mBEA.sum()) - (mBG0.sum() + mBE0.sum()))
         
     def PerformEstimation(self):
         print('Estimating the nested model (null hypothesis):')
-        self.estimator_res.PerformEstimation()
+        self.estimator0.PerformEstimation()
         print('Estimating the parent model (alternative hypothesis):')
-        self.estimator_unres.PerformEstimation()
+        self.estimatorA.PerformEstimation()
         # perform likelihood-ratio test
         self.PerformLRT()
     
     def IsConverged(self):
-        return (self.estimator_res.IsConverged() & self.estimator_unres.IsConverged())
+        return (self.estimator0.IsConverged() & self.estimatorA.IsConverged())
     
     def IsDone(self):
-        return (self.estimator_res.IsDone() & self.estimator_unres.IsDone())
+        return (self.estimator0.IsDone() & self.estimatorA.IsDone())
     
     def PerformLRT(self):
         print('Performing likelihood-ratio test with ' + str(self.iDF) + ' degrees of freedom:')
         if not(self.IsConverged()):
             raise RuntimeError('Estimates of the models have not converged.')
-        self.dTestStat = -2*(self.estimator_res.dLogL - self.estimator_unres.dLogL)
+        self.dTestStat = -2*(self.estimator0.dLogL - self.estimatorA.dLogL)
         print('Chi-square test statistic is ' + str(self.dTestStat))
         self.dPval = 1-chi2.cdf(self.dTestStat, self.iDF)
         print('P-value = ' + str(self.dPval))
