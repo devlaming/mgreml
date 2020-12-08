25c58d1697dec1f19755a6f42afa28a114e5d1f7
devlaming <devlaming@protonmail.com>
2020-12-08 16:25:23 +0100

    Get starting time of MGREML and pass along to MgremlReader

--- mgreml/__main__.py
+++ mgreml/__main__.py
@@ -3,15 +3,18 @@ perform mgreml with set of input arguments
 """
 import argparse
 import logging
+import time
 from data import reader
 
 def main():
+    # store starting time
+    dStart = time.time()
     # initialise parser
     parser = argparse.ArgumentParser()
     # initialise logger
     logger = logging.getLogger(__name__)
     # read in data
-    myMgremlData = reader.MgremlReader(parser, logger)
+    myMgremlData = reader.MgremlReader(parser, logger, dStart)
 
 if __name__ == '__main__':
     main()
