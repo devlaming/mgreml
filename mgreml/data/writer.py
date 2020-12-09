3e50efa14ee38093d8bf1d67e47afaacebe2354b
devlaming <devlaming@protonmail.com>
2020-12-09 17:28:17 +0100

    Changed prefix position in filenames DataWriter()
    so prefix can also contain the name of a directory, e.g. sPrefix = './output/test'

--- mgreml/data/writer.py
+++ mgreml/data/writer.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
-from mgreml.analysis import comparison
-from mgreml.analysis import estimator
+from analysis import comparison
+from analysis import estimator
 
 class DataWriter:
     
-    sPath = './output/'
     sExtension = 'out'
     sH2 = 'HSq.'
     sRG = 'RhoG.'
@@ -46,8 +45,8 @@ class DataWriter:
             dfHSq0 = pd.DataFrame(vHSq0,index=lPhenos)
             dfHSqA = pd.DataFrame(vHSqA,index=lPhenos)
             # set filenames
-            sHSq0 = DataWriter.sPath + DataWriter.sH2 + DataWriter.sH0 + self.sPrefix + DataWriter.sExtension
-            sHSqA = DataWriter.sPath + DataWriter.sH2 + DataWriter.sHA + self.sPrefix + DataWriter.sExtension
+            sHSq0 = self.sPrefix + DataWriter.sH2 + DataWriter.sH0 + DataWriter.sExtension
+            sHSqA = self.sPrefix + DataWriter.sH2 + DataWriter.sHA + DataWriter.sExtension
             # write dataframes
             dfHSq0.to_csv(sHSq0)
             dfHSqA.to_csv(sHSqA)
@@ -60,8 +59,8 @@ class DataWriter:
                 dfHSq0SE = pd.DataFrame(vHSq0SE,index=lPhenos)
                 dfHSqASE = pd.DataFrame(vHSqASE,index=lPhenos)
                 # set filenames
-                sHSq0SE = DataWriter.sPath + DataWriter.sH2 + DataWriter.sH0 + DataWriter.sSE + self.sPrefix + DataWriter.sExtension
-                sHSqASE = DataWriter.sPath + DataWriter.sH2 + DataWriter.sHA + DataWriter.sSE + self.sPrefix + DataWriter.sExtension
+                sHSq0SE = self.sPrefix + DataWriter.sH2 + DataWriter.sH0 + DataWriter.sSE + DataWriter.sExtension
+                sHSqASE = self.sPrefix + DataWriter.sH2 + DataWriter.sHA + DataWriter.sSE + DataWriter.sExtension
                 # write dataframes
                 dfHSq0SE.to_csv(sHSq0SE)
                 dfHSqASE.to_csv(sHSqASE)
@@ -73,7 +72,7 @@ class DataWriter:
             # set dataframe
             dfHSq = pd.DataFrame(vHSq,index=lPhenos)
             # set filename
-            sHSq = DataWriter.sPath + DataWriter.sH2 + self.sPrefix + DataWriter.sExtension
+            sHSq = self.sPrefix + DataWriter.sH2 + DataWriter.sExtension
             # write dataframe
             dfHSq.to_csv(sHSq)
             # if SEs are desired, store them
@@ -83,7 +82,7 @@ class DataWriter:
                 # set dataframe
                 dfHSqSE = pd.DataFrame(vHSqSE,index=lPhenos)
                 # set filenames
-                sHSqSE = DataWriter.sPath + DataWriter.sH2 + DataWriter.sSE + self.sPrefix + DataWriter.sExtension
+                sHSqSE = self.sPrefix + DataWriter.sH2 + DataWriter.sSE + DataWriter.sExtension
                 # write dataframes
                 dfHSqSE.to_csv(sHSqSE)
 
@@ -91,7 +90,7 @@ class DataWriter:
         if not(self.bNested):
             raise TypeError('Trying to write results for likelihood-ratio test, while no nested model has been estimated')
         # set filename
-        sLRTfile = DataWriter.sPath + DataWriter.sLRT + self.sPrefix + DataWriter.sExtension
+        sLRTfile = self.sPrefix + DataWriter.sLRT + DataWriter.sExtension
         with open(sLRTfile, 'w') as oLRTfile:
             oLRTfile.write('Results of likelihood-ratio test with ' + str(self.estimates.iDF) + ' degrees of freedom:\n')
             oLRTfile.write('Chi-square test statistic is ' + str(self.estimates.dTestStat) + '\n')
@@ -136,10 +135,10 @@ class DataWriter:
             dfSamplingVA = pd.DataFrame(self.estimates.estimatorA.mSamplingV, index=[lAllPhenosA,lAllFA], columns=[lAllPhenosA,lAllFA])
             dfSamplingV0 = pd.DataFrame(self.estimates.estimator0.mSamplingV, index=[lAllPhenos0,lAllF0], columns=[lAllPhenos0,lAllF0])
             # set output names
-            sParams0 = DataWriter.sPath + DataWriter.sCoeff + DataWriter.sH0 + self.sPrefix + DataWriter.sExtension
-            sParamsA = DataWriter.sPath + DataWriter.sCoeff + DataWriter.sHA + self.sPrefix + DataWriter.sExtension
-            sSamplingV0 = DataWriter.sPath + DataWriter.sCoeffvar + DataWriter.sH0 + self.sPrefix + DataWriter.sExtension
-            sSamplingVA = DataWriter.sPath + DataWriter.sCoeffvar + DataWriter.sHA + self.sPrefix + DataWriter.sExtension
+            sParams0 = self.sPrefix + DataWriter.sCoeff + DataWriter.sH0 + DataWriter.sExtension
+            sParamsA = self.sPrefix + DataWriter.sCoeff + DataWriter.sHA + DataWriter.sExtension
+            sSamplingV0 = self.sPrefix + DataWriter.sCoeffvar + DataWriter.sH0 + DataWriter.sExtension
+            sSamplingVA = self.sPrefix + DataWriter.sCoeffvar + DataWriter.sHA + DataWriter.sExtension
             # write dataframes
             dfParamsA.to_csv(sParamsA)
             dfParams0.to_csv(sParams0)
@@ -168,8 +167,8 @@ class DataWriter:
             dfParams = pd.DataFrame(vParam, index=[lAllPhenos,lAllF])
             dfSamplingV = pd.DataFrame(self.estimates.mSamplingV, index=[lAllPhenos,lAllF], columns=[lAllPhenos,lAllF])
             # set output names
-            sParams = DataWriter.sPath + DataWriter.sCoeff + self.sPrefix + DataWriter.sExtension
-            sSamplingV = DataWriter.sPath + DataWriter.sCoeffvar + self.sPrefix + DataWriter.sExtension
+            sParams = self.sPrefix + DataWriter.sCoeff + DataWriter.sExtension
+            sSamplingV = self.sPrefix + DataWriter.sCoeffvar + DataWriter.sExtension
             # write dataframes
             dfParams.to_csv(sParams)
             dfSamplingV.to_csv(sSamplingV)
@@ -177,8 +176,8 @@ class DataWriter:
     def WriteLogLik(self):
         if self.bNested:
             # set filenames
-            sLL0 = DataWriter.sPath + DataWriter.sLL + DataWriter.sH0 + self.sPrefix + DataWriter.sExtension
-            sLLA = DataWriter.sPath + DataWriter.sLL + DataWriter.sHA + self.sPrefix + DataWriter.sExtension
+            sLL0 = self.sPrefix + DataWriter.sLL + DataWriter.sH0 + DataWriter.sExtension
+            sLLA = self.sPrefix + DataWriter.sLL + DataWriter.sHA + DataWriter.sExtension
             with open(sLL0, 'w') as oLLfile:
                 oLLfile.write('Log-likelihood of nested model (null hypothesis) = ' + str(self.estimates.estimator0.dLogL) + ',\n')
                 oLLfile.write('based on data on ' + str(self.estimates.estimator0.mgreml_model.data.iT) + ' traits and ' + str(self.estimates.estimator0.mgreml_model.data.iN) + ' observations,\n')
@@ -199,7 +198,7 @@ class DataWriter:
                     oLLfile.write('Estimates converged after ' + str(self.estimates.estimatorA.iIter) + ' Newton iterations \n')
         else:
             # set filenames
-            sLL = DataWriter.sPath + DataWriter.sLL + self.sPrefix + DataWriter.sExtension
+            sLL = self.sPrefix + DataWriter.sLL + DataWriter.sExtension
             with open(sLL, 'w') as oLLfile:
                 oLLfile.write('Log-likelihood of model = ' + str(self.estimates.dLogL) + ',\n')
                 oLLfile.write('based on data on ' + str(self.estimates.mgreml_model.data.iT) + ' traits and ' + str(self.estimates.mgreml_model.data.iN) + ' observations,\n')
@@ -241,10 +240,10 @@ class DataWriter:
             dfVarGLS0 = pd.DataFrame(mVarGLS0, index=[indAllPhenos,indAllCovs], columns=[indAllPhenos,indAllCovs])
             dfVarGLSA = pd.DataFrame(mVarGLSA, index=[indAllPhenos,indAllCovs], columns=[indAllPhenos,indAllCovs])
             # set output names
-            sBetaGLS0 = DataWriter.sPath + DataWriter.sGLSest + DataWriter.sH0 + self.sPrefix + DataWriter.sExtension
-            sBetaGLSA = DataWriter.sPath + DataWriter.sGLSest + DataWriter.sHA + self.sPrefix + DataWriter.sExtension
-            sVarGLS0 = DataWriter.sPath + DataWriter.sGLSvar + DataWriter.sH0 + self.sPrefix + DataWriter.sExtension
-            sVarGLSA = DataWriter.sPath + DataWriter.sGLSvar + DataWriter.sHA + self.sPrefix + DataWriter.sExtension
+            sBetaGLS0 = self.sPrefix + DataWriter.sGLSest + DataWriter.sH0 + DataWriter.sExtension
+            sBetaGLSA = self.sPrefix + DataWriter.sGLSest + DataWriter.sHA + DataWriter.sExtension
+            sVarGLS0 = self.sPrefix + DataWriter.sGLSvar + DataWriter.sH0 + DataWriter.sExtension
+            sVarGLSA = self.sPrefix + DataWriter.sGLSvar + DataWriter.sHA + DataWriter.sExtension
             # write dataframes
             dfBetaGLS0.to_csv(sBetaGLS0)
             dfBetaGLSA.to_csv(sBetaGLSA)
@@ -276,8 +275,8 @@ class DataWriter:
             dfBetaGLS = pd.DataFrame(vBetaGLS, index=[indAllPhenos,indAllCovs])
             dfVarGLS = pd.DataFrame(mVarGLS, index=[indAllPhenos,indAllCovs], columns=[indAllPhenos,indAllCovs])
             # set output names
-            sBetaGLS = DataWriter.sPath + DataWriter.sGLSest + self.sPrefix + DataWriter.sExtension
-            sVarGLS = DataWriter.sPath + DataWriter.sGLSvar + self.sPrefix + DataWriter.sExtension
+            sBetaGLS = self.sPrefix + DataWriter.sGLSest + DataWriter.sExtension
+            sVarGLS = self.sPrefix + DataWriter.sGLSvar + DataWriter.sExtension
             # write dataframes
             dfBetaGLS.to_csv(sBetaGLS)
             dfVarGLS.to_csv(sVarGLS)
@@ -297,10 +296,10 @@ class DataWriter:
             dfRhoGA = pd.DataFrame(mRhoGA,index=lPhenos,columns=lPhenos)
             dfRhoEA = pd.DataFrame(mRhoEA,index=lPhenos,columns=lPhenos)
             # set filenames
-            sRhoG0 = DataWriter.sPath + DataWriter.sRG + DataWriter.sH0 + self.sPrefix + DataWriter.sExtension
-            sRhoE0 = DataWriter.sPath + DataWriter.sRE + DataWriter.sH0 + self.sPrefix + DataWriter.sExtension
-            sRhoGA = DataWriter.sPath + DataWriter.sRG + DataWriter.sHA + self.sPrefix + DataWriter.sExtension
-            sRhoEA = DataWriter.sPath + DataWriter.sRE + DataWriter.sHA + self.sPrefix + DataWriter.sExtension
+            sRhoG0 = self.sPrefix + DataWriter.sRG + DataWriter.sH0 + DataWriter.sExtension
+            sRhoE0 = self.sPrefix + DataWriter.sRE + DataWriter.sH0 + DataWriter.sExtension
+            sRhoGA = self.sPrefix + DataWriter.sRG + DataWriter.sHA + DataWriter.sExtension
+            sRhoEA = self.sPrefix + DataWriter.sRE + DataWriter.sHA + DataWriter.sExtension
             # write dataframes
             dfRhoG0.to_csv(sRhoG0)
             dfRhoE0.to_csv(sRhoE0)
@@ -319,10 +318,10 @@ class DataWriter:
                 dfRhoGASE = pd.DataFrame(mRhoGASE,index=lPhenos,columns=lPhenos)
                 dfRhoEASE = pd.DataFrame(mRhoEASE,index=lPhenos,columns=lPhenos)
                 # set filenames
-                sRhoG0SE = DataWriter.sPath + DataWriter.sRG + DataWriter.sH0 + DataWriter.sSE + self.sPrefix + DataWriter.sExtension
-                sRhoE0SE = DataWriter.sPath + DataWriter.sRE + DataWriter.sH0 + DataWriter.sSE + self.sPrefix + DataWriter.sExtension
-                sRhoGASE = DataWriter.sPath + DataWriter.sRG + DataWriter.sHA + DataWriter.sSE + self.sPrefix + DataWriter.sExtension
-                sRhoEASE = DataWriter.sPath + DataWriter.sRE + DataWriter.sHA + DataWriter.sSE + self.sPrefix + DataWriter.sExtension
+                sRhoG0SE = self.sPrefix + DataWriter.sRG + DataWriter.sH0 + DataWriter.sSE + DataWriter.sExtension
+                sRhoE0SE = self.sPrefix + DataWriter.sRE + DataWriter.sH0 + DataWriter.sSE + DataWriter.sExtension
+                sRhoGASE = self.sPrefix + DataWriter.sRG + DataWriter.sHA + DataWriter.sSE + DataWriter.sExtension
+                sRhoEASE = self.sPrefix + DataWriter.sRE + DataWriter.sHA + DataWriter.sSE + DataWriter.sExtension
                 # write dataframes
                 dfRhoG0SE.to_csv(sRhoG0SE)
                 dfRhoE0SE.to_csv(sRhoE0SE)
@@ -338,8 +337,8 @@ class DataWriter:
             dfRhoG = pd.DataFrame(mRhoG,index=lPhenos,columns=lPhenos)
             dfRhoE = pd.DataFrame(mRhoE,index=lPhenos,columns=lPhenos)
             # set filenames
-            sRhoG = DataWriter.sPath + DataWriter.sRG + self.sPrefix + DataWriter.sExtension
-            sRhoE = DataWriter.sPath + DataWriter.sRE + self.sPrefix + DataWriter.sExtension
+            sRhoG = self.sPrefix + DataWriter.sRG + DataWriter.sExtension
+            sRhoE = self.sPrefix + DataWriter.sRE + DataWriter.sExtension
             # write dataframes
             dfRhoG.to_csv(sRhoG)
             dfRhoE.to_csv(sRhoE)
@@ -352,8 +351,8 @@ class DataWriter:
                 dfRhoGSE = pd.DataFrame(mRhoGSE,index=lPhenos,columns=lPhenos)
                 dfRhoESE = pd.DataFrame(mRhoESE,index=lPhenos,columns=lPhenos)
                 # set filenames
-                sRhoGSE = DataWriter.sPath + DataWriter.sRG + DataWriter.sSE + self.sPrefix + DataWriter.sExtension
-                sRhoESE = DataWriter.sPath + DataWriter.sRE + DataWriter.sSE + self.sPrefix + DataWriter.sExtension
+                sRhoGSE = self.sPrefix + DataWriter.sRG + DataWriter.sSE + DataWriter.sExtension
+                sRhoESE = self.sPrefix + DataWriter.sRE + DataWriter.sSE + DataWriter.sExtension
                 # write dataframes
                 dfRhoGSE.to_csv(sRhoGSE)
                 dfRhoESE.to_csv(sRhoESE)
