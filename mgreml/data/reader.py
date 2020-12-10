a616573f6480a2a39dff10ad209ef9183f97c271
devlaming <devlaming@protonmail.com>
2020-12-10 14:47:01 +0100

    Harmonised all functions to read in data w.r.t. main branch
    Outstanding issue: harmonise reading in other input args (e.g. --no-se) w.r.t. main branch

--- mgreml/data/reader.py
+++ mgreml/data/reader.py
@@ -44,87 +44,75 @@ class MgremlReader:
     sGen = 'gen'
     sEnv = 'env'
     
+    # set boolean for restricted models
+    bNull = True
+    
+    # set default values for dropping leading and trailing PCs
+    iDropLeadPCsDefault = 20
+    iDropTrailPCsDefault = 0
+    
     def __init__(self, parser, logger):
-        '''
-        MgremlReader is provided with a logger and a parser.
-        
-        The logger is used to print to console and log-file what's happening.
-        
-        The parser is used to read out e.g. commands along the following lines:
-        python mgreml --grm mygrm --pheno mypheno --out results
-        
-        An instance of an MgremlReader is used as input
-        when initialising instances of other classes involved in
-        MGREML estimation; the attributes of the MgremlReader will be 
-        used to determine what needs to be done
-        '''
         # store logger and parser as attributes of instance
         self.logger = logger
         self.parser = parser
         # initialise arguments and logger
         self.InitialiseArgumentsAndLogger()
         # print welcome screen and given input args
-        self.PrintStart()
+        self.PrintWelcome()
         # assume we will carry out an analysis
         self.bAnalyse = True
-        # if either GRM, pheno or output has not been specified
-        # do not analyse anything
+        # if GRM, pheno or output has not been specified: stop analysing
         if self.args.grm is None:
-            self.logger.warning('Error: no GRM has been specified.')
+            self.logger.warning('Error: no GRM was specified.')
             self.bAnalyse = False
         if self.args.pheno is None:
-            self.logger.warning('Error: no phenotype file has been specified.')
+            self.logger.warning('Error: no phenotype file was specified.')
             self.bAnalyse = False            
         if self.args.out is None:
-            self.logger.warning('Error: no prefix for output files has been specified.')
+            self.logger.warning('Error: no prefix for the output files was specified.')
             self.bAnalyse = False
         # if we can analyse, read in data
         if self.bAnalyse:
-            # read GRM
-            self.read_grm()
             # read phenotype file
-            self.read_data(MgremlReader.sPhe)
+            self.ReadData(MgremlReader.sPhe)
             # if covariate file specified: read
             if self.args.covar is not None:
-                self.read_data(MgremlReader.sCov)
+                self.ReadData(MgremlReader.sCov)
             else:
                 self.dfX = None
             # if covar model has been specified: read
             if self.args.covar_model is not None:
-                self.dfBinXY = self.read_model(component = MgremlReader.sCov)
+                self.ReadModel(MgremlReader.sCov)
             else:
                 self.dfBinXY = None
             # if genetic model has been specified: read
             if self.args.genetic_model is not None:
-                self.dfGenBinFY = self.read_model(component = MgremlReader.sGen)
+                self.ReadModel(MgremlReader.sGen)
             else:
                 self.dfGenBinFY = None
             # if genetic model under null has been specified: read
             if self.args.restricted_genetic_model is not None:
-                self.dfGenBinFY0 = self.read_model(component = MgremlReader.sGen, restricted = True)
+                self.ReadModel(MgremlReader.sGen, MgremlReader.bNull)
             else:
                 self.dfGenBinFY0 = None
             # if environment model has been specified: read
             if self.args.environment_model is not None:
-                self.dfEnvBinFY = self.read_model(component = MgremlReader.sEnv)
+                self.ReadModel(MgremlReader.sEnv)
             else:
                 self.dfEnvBinFY = None
             # if genetic model under null has been specified: read
             if self.args.restricted_environment_model is not None:
-                self.dfEnvBinFY0 = self.read_model(component = MgremlReader.sEnv, restricted = True)
+                self.ReadModel(MgremlReader.sEnv, MgremlReader.bNull)
             else:
                 self.dfEnvBinFY0 = None
-        # self.sPrefix        # string based on --out results
-        # self.dfY            # DataFrame based on --pheno mypheno.txt [nolabelpheno]
-        # self.dfX            # DataFrame based on --covar mycovar.txt; DEFAULT = None
-        # self.dfBinXY        # DataFrame based on --covar-model mycovmodel.txt [nolabelpheno] [nolabelcovar]; DEFAULT = None
-        # self.dfGenBinFY     # DataFrame based on --genetic-model mygenmodel.txt [nolabelpheno] [nolabelfactor]; DEFAULT = None
-        # self.dfEnvBinFY     # DataFrame based on--environment-model myenvmodel.txt [nolabelpheno] [nolabelfactor]; DEFAULT = None
+            # read GRM
+            self.ReadGRM()
+            # set number of leading and trailing PCs
+            self.iDropLeadPCs = MgremlReader.iDropLeadPCsDefault
+            self.iDropTrailPCs = MgremlReader.iDropTrailPCsDefault            
         # self.bPerfectRhoG   # Boolean based on --rho-genetic 1; DEFAULT = False
         # self.bNoRhoG        # Boolean based on --rho-genetic 0; DEFAULT = False
         # self.bNoRhoE        # Boolean based on --rho-environment 0; DEFAULT = False
-        # self.dfGenBinFY0    # DataFrame based on --restricted-genetic-model mygenmodel0.txt [nolabelpheno] [nolabelfactor]; DEFAULT = None
-        # self.dfEndBinFY0    # DataFrame based on --restricted-environment-model myenvmodel0.txt [nolabelpheno] [nolabelfactor]; DEFAULT = None
         # self.bPerfectRhoG0  # Boolean based on --restricted-rho-genetic 1; DEFAULT = False
         # self.bNoRhoG0       # Boolean based on --restricted-rho-genetic 0; DEFAULT = False
         # self.bNoRhoE0       # Boolean based on --restricted-rho-environment 0; DEFAULT = False
@@ -196,9 +184,10 @@ class MgremlReader:
             # parse the input options
             self.args = self.parser.parse_args()
         except Exception:
-            self.logger.error('Wrong input options used: MGREML cannot not parse input options')
+            # if doesn't work: print error
+            self.logger.error('Error: you specified incorrect input options.')
             raise ValueError
-        # customise the logger e.g. usings args.out
+        # customise the logger using the prefix for output-files
         c_handler = logging.StreamHandler()
         # if no --out option has not been specified
         # use generic name for log-file and so on
@@ -208,20 +197,22 @@ class MgremlReader:
         else:
             self.sPrefix = 'output.'
             sFileOut = self.sPrefix + 'log'
+        # set the name for the log file
         f_handler = logging.FileHandler(sFileOut,'w+',encoding="utf-8")
         c_handler.setLevel(logging.DEBUG)
         f_handler.setLevel(logging.DEBUG)
         self.logger.setLevel(logging.DEBUG)
-        # Create formatters and add it to handlers
+        # for output to the console, just print warnings, info, errors, etc.
         c_format = logging.Formatter('%(message)s')
+        # for output to the log file also add timestamps
         f_format = logging.Formatter('%(message)s - %(asctime)s ')
         c_handler.setFormatter(c_format)
         f_handler.setFormatter(f_format)
-        # Add handlers to the logger
+        # add handlers to the logger
         self.logger.addHandler(c_handler)
         self.logger.addHandler(f_handler)
         
-    def PrintStart(self):
+    def PrintWelcome(self):
         # try to print welcome screen
         try:
             defaults = vars(self.parser.parse_args(''))
@@ -235,17 +226,17 @@ class MgremlReader:
             header = header[0:-1]+'\n'
             self.logger.info(header)
         except Exception:
-            self.logger.error('Wrong input values used: MGREML does not understand input options')
+            self.logger.error('Error: you specified incorrect input options.')
             raise ValueError
 
-    def read_grm(self):
+    def ReadGRM(self):
         # set filenames for reading binary GRM
         BinFileName = self.args.grm + ".grm.bin"
         NFileName = self.args.grm + ".grm.N.bin"
         IDFileName = self.args.grm + ".grm.id"
         # check if one or more files are missing:
         if not(os.path.isfile(BinFileName)) or not(os.path.isfile(NFileName)) or not(os.path.isfile(IDFileName)):
-            self.logger.error('Specified set of GRM files either incomplete or does not exist')
+            self.logger.error('Error: specified set of GRM files either incomplete or non-existent.')
             raise ValueError
         # read IDs and sample size
         ids = pd.read_csv(IDFileName, sep = '\t', header = None)
@@ -274,12 +265,12 @@ class MgremlReader:
                 k += 1
         # convert to pd dataframe        
         self.dfA = pd.DataFrame(mA, columns=index, index=index)
-        print('Printing first and last few rows of GRM')
-        print(self.dfA)
+        # print update
         self.logger.info('Finished reading in GRM')
         
-    def read_data(self, sType):
-        ''' read data '''
+    def ReadData(self, sType):
+        # figure out what type of input data we have
+        # and set strings appropriately
         if sType == MgremlReader.sPhe:
             sNoLabel = 'nolabelpheno'
             sData = 'phenotype'
@@ -291,18 +282,24 @@ class MgremlReader:
             sOption = '--covar'
             lArgs = self.args.covar
         else:
-            raise ValueError('MgremlReader.read_data() can only be used to read a phenotype file or covariate file')
-        if len(lArgs) == 1: # if no. of input args. is one
-            bHeader = True # we have a header
+            self.logger.error('Error: MgremlReader.ReadData() can only be used to read a phenotype file or covariate file. How did you even get here?')
+            raise ValueError
+        # if no. of input args. is one
+        if len(lArgs) == 1:
+            # we have a header
+            bHeader = True
         elif len(lArgs)==2: # if it's 2
-            if lArgs[1] != sNoLabel: # but the 2nd arg. != no-label string
+            # but the 2nd arg. != no-label string
+            if lArgs[1] != sNoLabel:
                 # raise an error
-                raise ValueError('The second argument of the ' + sOption + ' flag is incorrectly specified, did you mean ' + sNoLabel + '?')
+                self.logger.error('Error: the second argument of ' + sOption + ' is incorrect. Did you mean ' + sNoLabel + '?')
+                raise ValueError
             # otherwise: we do not have a header
             bHeader=False
         else:
             # if we have neither 1 nor 2 input args: raise an errror
-            raise ValueError('The ' + sOption + ' flag requires a filename and the optional modifier ' + sNoLabel + '.')
+            self.logger.error('Error: ' + sOption + ' requires a filename and can have the optional argument ' + sNoLabel + '.')
+            raise ValueError
         # prepare string to let user know whether header has been set, yes or no
         if bHeader:
             sInfoString = 'Headers specified in ' + sData + ' file\nUsing headers to set labels'
@@ -313,26 +310,138 @@ class MgremlReader:
         # check if the data file is missing:
         if not(os.path.isfile(lArgs[0])):
             # if so raise an error
-            self.logger.error('Specified ' + sData + ' file does not exist')
+            self.logger.error('Error: specified ' + sData + ' file does not exist')
             raise ValueError
         # report whether header has been set, yes or no
         self.logger.info(sInfoString)
+        # indicate data is being read
         self.logger.info('Reading ' + sData + ' file {f}'.format(f=lArgs[0]))
+        # read data
         dfData = pd.read_csv(lArgs[0], index_col = MgremlReader.lIndFID_IID, header = iHeader, sep = "\s+|\t+|,",engine='python')
+        # force missings to NaN
         dfData.apply(pd.to_numeric, errors='coerce')
+        # get number of observations and traits, and report
         (iN,iT) = dfData.shape
         self.logger.info(sData + ' file contains data on {N} individuals and {T} '.format(N=iN,T=iT) + sData + 's')
-        #find number of NA  write number of missings
+        # find number of NaNs and report
         iMiss = dfData.isnull().sum().sum()
         self.logger.info('Encountered {M} missings'.format(M=iMiss))
+        # if no header has been specified
         if not(bHeader):
+            # set labels of the multiindex to FID, IID
             dfData.index.names = MgremlReader.lLabelsFID_IID
+            # set labels of trait/covariates to phenotype/covariate 1,
+            # phenotype/covariate 2, etc.
             dfData.columns = [sData + str(x) for x in range(0,iT)]
-        print('Printing first and last few rows of ' + sData + ' data')
-        print(dfData)
+        # store data as appropriate attribute of MgremlReader instance
         if sType == MgremlReader.sPhe:
             self.dfY = dfData
         else:
             self.dfX = dfData
+        # report reading data is done
         self.logger.info('Finished reading in ' + sData + ' data')
-        
+
+    def ReadModel(self, sType, bNull = False):
+        # set no label string for phenotypes (in rows of all models)
+        sNoLabelIndex = 'nolabelpheno'
+        # figure out what type of input data we have
+        # and set strings appropriately
+        if sType == MgremlReader.sCov:
+            sNoLabelHeader = 'nolabelcovar'
+            sData = 'covariate model'
+            sDescr = 'covariates'
+            sOption = '--covar-model'
+            lArgs = self.args.covar_model
+        elif sType == MgremlReader.sGen:
+            sNoLabelHeader = 'nolabelfactor'
+            sDescr = 'genetic factors'
+            if bNull:
+                sData = 'restricted genetic model'
+                sOption = '--restricted-genetic-model'
+                lArgs = self.args.restricted_genetic_model
+            else:
+                sData = 'genetic model'
+                sOption = '--genetic-model'
+                lArgs = self.args.genetic_model
+        elif sType == MgremlReader.sEnv:
+            sNoLabelHeader = 'nolabelfactor'
+            sDescr = 'environment factors'
+            if bNull:
+                sData = 'restricted environment model'
+                sOption = '--restricted-environment-model'
+                lArgs = self.args.restricted_environment_model
+            else:
+                sData = 'environment model'
+                sOption = '--environment-model'
+                lArgs = self.args.environment_model
+        else:
+            self.logger.error('Error: MgremlReader.ReadModel() can only be used to read a model specification for factors or covariates. How did you even get here?')
+            raise ValueError
+        # if no. of input args. is one
+        if len(lArgs) == 1:
+            # we have a header and index
+            bHeader = True
+            bIndex = True
+        elif len(lArgs)==2: # if it's 2
+            if lArgs[1] == sNoLabelIndex:
+                # we have a header but no index
+                bHeader = True
+                bIndex = False
+            elif lArgs[1] == sNoLabelHeader:
+                # we have an index but no header
+                bHeader = False
+                bIndex = True
+            else:
+                # raise an error
+                self.logger.error('Error: the second argument of ' + sOption + ' is incorrect. Did you mean ' + sNoLabelIndex + ' or ' + sNoLabelHeader + '?')
+                raise ValueError
+        elif len(lArgs)==3: # if it's 3
+            if (sNoLabelIndex in lArgs[1:]) and (sNoLabelHeader in lArgs[1:]):
+                # we have neither index nor header
+                bHeader = False
+                bIndex = False
+            else:
+                # raise an error
+                self.logger.error('Error: the second or third argument of ' + sOption + ' is incorrect. Did you mean ' + sNoLabelIndex + ' ' + sNoLabelHeader + '?')
+                raise ValueError
+        else:
+            # if we have neither 1, 2, nor 3 input args: raise an errror
+            self.logger.error('Error: ' + sOption + ' requires a filename and can have the optional arguments ' + sNoLabelIndex + ' and/or ' + sNoLabelHeader + '.')
+            raise ValueError
+        # assign False/None as column/row where indices/headers are found
+        iIndCol = False
+        iHeadRow = None
+        # if either available, set to first column resp. row
+        if bIndex:
+            iIndCol = 0
+        if bHeader:
+            iHeadRow = 0
+        # check if the data file is missing:
+        if not(os.path.isfile(lArgs[0])):
+            # if so raise an error
+            self.logger.error('Error: specified file for the ' + sData + ' does not exist')
+            raise ValueError
+        self.logger.info('Reading {S} file {f}'.format(S=sData,f=lArgs[0]))
+        # read the file
+        dfBin = pd.read_csv(lArgs[0], header = iHeadRow, sep = "\s+|\t+|,", engine = 'python', index_col = iIndCol)
+        # set missings to NaN
+        dfBin = dfBin.apply(pd.to_numeric, errors='coerce')
+        # get the number of traits and factors/covariates involved and report
+        (iT,iK) = dfBin.shape
+        print(dfBin)
+        self.logger.info('Found a ' + sData + ' on ' + str(iT) + ' phenotypes and ' + str(iK) + ' ' + sDescr + '.')
+        # store dataframe as appropriate attribute of MgremlReader
+        if sType == MgremlReader.sCov:
+            self.dfBinXY = dfBin
+        elif sType == MgremlReader.sGen:
+            if bNull:
+                self.dfGenBinFY0 = dfBin
+            else:
+                self.dfGenBinFY = dfBin
+        else:
+            if bNull:
+                self.dfEnvBinFY0 = dfBin
+            else:
+                self.dfEnvBinFY = dfBin
+        # report reading data is done
+        self.logger.info('Finished reading in the ' + sData)
