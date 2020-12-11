4a0f6a36937a179a5c69224a3db743dba46a5d75
devlaming <devlaming@protonmail.com>
2020-12-11 17:04:52 +0100

    Major revisions of comparison.py
    1/ introduced logger to store/show updates
    2/ changed structure of input arguments of __init__ method; all information now deducted from MgremlData instance

--- mgreml/analysis/comparison.py
+++ mgreml/analysis/comparison.py
@@ -5,14 +5,19 @@ from analysis import estimator
 
 class NestedEstimators:
     
-    def __init__(self, mdData, dfGenBinFY0 = None, dfEnvBinFY0 = None, dfGenBinFY = None, dfEnvBinFY = None, bBFGS = True, bStoreIters= False, bSEs = False, bReturnFullModelSpecs = False):
-        self.estimatorA = estimator.MgremlEstimator(mdData, dfGenBinFY, dfEnvBinFY, bBFGS, bStoreIters, bSEs, bReturnFullModelSpecs)
-        self.estimator0 = estimator.MgremlEstimator(mdData, dfGenBinFY0, dfEnvBinFY0, bBFGS, bStoreIters, bSEs, bReturnFullModelSpecs)
+    def __init__(self, mdData):
+        # store the logger
+        self.logger = mdData.logger
+        self.logger.info('INITIALSING RESTRICTED MODEL (NULL HYPOTHESIS)')
+        self.estimator0 = estimator.MgremlEstimator(mdData, bNested = True)
+        self.logger.info('INITIALSING ALTERNATIVE MODEL')
+        self.estimatorA = estimator.MgremlEstimator(mdData, bNested = False)
         # initialisation of StructuralModel instances using
         # same mdData guarantees that the these StructuralModels
         # consider the same set of traits, in the same order
         # checking whether models are nest, is thus only about
         # checking factors and their free coefficients
+        self.logger.info('CHECKING WHETHER MODELS ARE NESTED')
         self.CheckModelsNested()
         
     def CheckModelsNested(self):
@@ -50,11 +55,12 @@ class NestedEstimators:
         if ((mBEA_aligned - mBE0) < 0).any():
             raise ValueError('There is at least one environment coefficient where the nested model is free and the alternative model is not')
         self.iDF = int((mBGA.sum() + mBEA.sum()) - (mBG0.sum() + mBE0.sum()))
+        self.logger.info('The model under the null hypothesis is nested with respect to the alternative model\n')
         
     def PerformEstimation(self):
-        print('Estimating the nested model (null hypothesis):')
+        self.logger.info('Estimating the nested model (null hypothesis):')
         self.estimator0.PerformEstimation()
-        print('Estimating the parent model (alternative hypothesis):')
+        self.logger.info('Estimating the alternative model:')
         self.estimatorA.PerformEstimation()
         # perform likelihood-ratio test
         self.PerformLRT()
@@ -66,13 +72,13 @@ class NestedEstimators:
         return (self.estimator0.IsDone() & self.estimatorA.IsDone())
     
     def PerformLRT(self):
-        print('Performing likelihood-ratio test with ' + str(self.iDF) + ' degrees of freedom:')
+        self.logger.info('Performing likelihood-ratio test with ' + str(self.iDF) + ' degrees of freedom:')
         if not(self.IsConverged()):
             raise RuntimeError('Estimates of the models have not converged.')
         self.dTestStat = -2*(self.estimator0.dLogL - self.estimatorA.dLogL)
-        print('Chi-square test statistic is ' + str(self.dTestStat))
+        self.logger.info('Chi-square test statistic is ' + str(self.dTestStat))
         self.dPval = 1-chi2.cdf(self.dTestStat, self.iDF)
-        print('P-value = ' + str(self.dPval))
+        self.logger.info('P-value = ' + str(self.dPval) + '\n')
         
         
         
